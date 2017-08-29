0677f3cc2ebf7219b3f6d3f0dbe398f217c5c787
kartul <derek.maekask@gmail.com>
2017-08-29 12:08:27 +0300

    Minor fix to password field validation + pep8

--- hamsters.py
+++ hamsters.py
@@ -15,27 +15,52 @@ NAME = "dbName"
 PASSWD = "dbPassword"
 PORT = "dbPort"
 
+
 def connect_db(host, port, user, password, db):
     try:
-        return pymysql.connect(host=host, port=port, user=user, passwd=password, db=db)
-    except pymysql.Error, e:
+        return pymysql.connect(
+            host=host,
+            port=port,
+            user=user,
+            passwd=password,
+            db=db)
+    except pymysql.Error as e:
         sys.stderr.write("[ERROR] % d: % s\n" % (e.args[0], e.args[1]))
         return False
 
 
 def main():
     parser = configargparse.ArgParser(description='Hamster wheel 9000')
-    parser.add_argument('-dir','--directory', help='Directory and filename of csv. ex: accounts/hive1.csv',required=True)
-    parser.add_argument('-w','--workers', help='amount of lines per csv',required=True)
-    parser.add_argument('-t','--table', help='db table to get hamsters from.',required=True)
+    parser.add_argument(
+        '-dir',
+        '--directory',
+        help='Directory and filename of csv. ex: accounts/hive1.csv',
+        required=True)
+    parser.add_argument(
+        '-w',
+        '--workers',
+        help='amount of lines per csv',
+        required=True)
+    parser.add_argument(
+        '-t',
+        '--table',
+        help='db table to get hamsters from.',
+        required=True)
     args = parser.parse_args()
 
-    QUERY = "SELECT auth,username,password FROM {} order by last_timestamp limit {};".format(args.table, args.workers)
-    UPDATEQUERY = "UPDATE {} set last_timestamp=now() order by last_timestamp limit {}".format(args.table, args.workers)
+    QUERY = "SELECT auth,username,password FROM {} order by last_timestamp limit {};".format(
+        args.table, args.workers)
+    UPDATEQUERY = "UPDATE {} set last_timestamp=now() order by last_timestamp limit {}".format(
+        args.table, args.workers)
     FILENAME = "{}".format(args.directory)
 
     db = connect_db(HOST, int(PORT), USER, PASSWD, NAME)
-    dump_writer = csv.writer(open(FILENAME, 'w'), delimiter=',', quoting=csv.QUOTE_NONE)
+    dump_writer = csv.writer(
+        open(
+            FILENAME,
+            'w'),
+        delimiter=',',
+        quoting=csv.QUOTE_NONE)
     cur = db.cursor()
     cur.execute(QUERY)
     for row in cur.fetchall():
@@ -49,6 +74,7 @@ def main():
 
     with open(FILENAME) as f:
         print "".join(line for line in f if not line.isspace())
+
 
 if __name__ == "__main__":
     main()
